a443c9ab3fa6e31a0133a9cd4025351301c886cc
Gilad Livshitz <giladliv123@gmail.com>
2022-12-15 11:24:40 +0200

    path parser class complete base structure

--- loginPage/app/src/main/java/db/PathParse.java
+++ loginPage/app/src/main/java/db/PathParse.java
@@ -1,19 +1,38 @@
 package db;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 
 public class PathParse
 {
-    Map<String, Object> mapPathVals;
+    Map<String, String> mapPathVals = new HashMap<>();
 
-    public PathParse(String path) {
-        //this.mapPathVals = mapPathVals;
-        ArrayList<String> dataParsed = ;
+    public PathParse(String path)
+    {
+        mapPathVals = parsePath(path);
     }
 
-    private Map<String, Object> parsePath(String path)
+    private Map<String, String> parsePath(String path)
     {
+        ArrayList<String> dataParsed = new ArrayList<>(Arrays.asList(path.split("[/]")));
+        Map<String, String> mapParsed = new HashMap<>();
 
+        // making the data parsed even length
+        if (dataParsed.size() % 2 == 1)
+            dataParsed.add("");
+
+        for (int i = 0; i < dataParsed.size(); i += 2)
+        {
+            mapParsed.put(dataParsed.get(i), dataParsed.get(i + 1));
+        }
+
+        return (mapParsed);
+    }
+    
+    public String getDataFromParsed(String key)
+    {
+        return this.mapPathVals.get(key);
     }
 }
