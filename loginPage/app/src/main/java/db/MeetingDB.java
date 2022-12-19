d8f1660623c60cf16238d3c1f100ca36fcc8db2a
Gilad Livshitz <giladliv123@gmail.com>
2022-12-19 17:10:30 +0200

    update from yudit

--- loginPage/app/src/main/java/db/MeetingDB.java
+++ loginPage/app/src/main/java/db/MeetingDB.java
@@ -9,6 +9,7 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
@@ -69,5 +70,57 @@ public class MeetingDB
                     .collection(IMeeting.DOCK_NAME).document(meeting.getMeetingId());
         }
         docRef.set(meeting);
+    }
+
+    public static ArrayList<Meeting> getStudentMeetings(String StudentId){
+    ArrayList<Meeting> meetings = new ArrayList<>();
+    if (StudentId == null || StudentId.isEmpty())
+        return meetings;
+
+    Query query = firestore.collection(IMeeting.DOCK_NAME).whereEqualTo("studentId",StudentId);
+    Task<QuerySnapshot> task = query.get();
+
+    DataCenterDB.waitTaskComplete(task);
+
+    if (task.isSuccessful()) {
+        for (QueryDocumentSnapshot document : task.getResult()) {
+            Log.d("QUERY_TEST", document.getId() + " => " + document.getData());
+            meetings.add(document.toObject(Meeting.class));
+        }
+    }
+    return meetings;
+}
+
+
+    public static ArrayList<Meeting> getTutorMeetings(String TutorId){
+        ArrayList<Meeting> meetings = new ArrayList<>();
+        if (TutorId == null || TutorId.isEmpty())
+            return meetings;
+        Query query = firestore.collection(IMeeting.DOCK_NAME).whereEqualTo("tutorId",TutorId);
+        Task<QuerySnapshot> task = query.get();
+
+        DataCenterDB.waitTaskComplete(task);
+
+        if (task.isSuccessful()) {
+            for (QueryDocumentSnapshot document : task.getResult()) {
+                Log.d("QUERY_TEST", document.getId() + " => " + document.getData());
+                meetings.add(document.toObject(Meeting.class));
+            }
+        }
+        return meetings;
+    }
+
+
+    public static Meeting getMeeting(String mID){
+        DocumentReference docRef = firestore.collection("meetings").document(mID);
+        Task<DocumentSnapshot> task = docRef.get();
+
+        DataCenterDB.waitTaskComplete(task);
+
+        DocumentSnapshot document = task.getResult();
+        if(!document.exists())
+            return null;
+        return document.toObject(Meeting.class);
+
     }
 }
